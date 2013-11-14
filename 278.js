249c314a2bf6c507ab88f3a867cf931ce5653044
Michael Cummings <michaelc@gwu.edu>
2013-11-14 15:47:57 -0500

    corrected milestone

--- 278.js
+++ 278.js
@@ -1,6 +1,6 @@
 // m14_004 278 
 //
-casper.test.begin('m4_004 #278 page loads with diacritics', 3, function suite(test) {
+casper.test.begin('m14_004 #278 page loads with diacritics', 3, function suite(test) {
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/7765894';
 var ENTITY=SERVER+USECASE;
