3e54625b6b9ae62b25231d31b9666bd550e5ccf0
Michael Cummings <michaelc@gwu.edu>
2013-11-14 15:48:44 -0500

    corrected milestone

--- 372.js
+++ 372.js
@@ -1,6 +1,6 @@
 // m14-004 and m14_003 371 + 372 
 //
-casper.test.begin('m4_003 and 004 #371+372 ISBN diacritic does not trigger error', 1, function suite(test) { 
+casper.test.begin('m14_003 and 004 #371+372 ISBN diacritic does not trigger error', 1, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/12280154';
 var ENTITY=SERVER+USECASE;
