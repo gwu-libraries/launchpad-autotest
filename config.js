efc525903df59778a7179b96978450f6da1bb434
Michael Cummings <michaelc@gwu.edu>
2013-11-14 10:03:07 -0500

    Added explanation how to reference the config.js file. Removed the LOG setting.

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 // config.js
 // Use this file to configure a server setting or other constants
-// pick ONE
+// This file should be included when calling casperjs
+// Example:
+// casperjs test $1.js --includes=config.js --xunit=log-$1.xml
+//
+// identity one SERVER path
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // var SERVER='http://gwfindit-prod.wrlc.org';
-var SERVER='http://gwfindit-test.wrlc.org';
 // var SERVER='http://gwdev-cummings.wrlc.org';
 // var SERVER='http://gwdev-edsu.wrlc.org';
-//
-// log option
-var LOG='xunit=log-$1.xml';
+var SERVER='http://gwfindit-test.wrlc.org';
 
