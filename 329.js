ff66ad921ed765040f581495c00db109608da90a
Michael Cummings <michaelc@gwu.edu>
2013-11-14 15:48:07 -0500

    corrected milestone

--- 329.js
+++ 329.js
@@ -1,6 +1,6 @@
 // m14_001 #329 
 //
-casper.test.begin('m4_001 #329 no GT holding for HU id', 1, function suite(test) { 
+casper.test.begin('m14_001 #329 no GT holding for HU id', 1, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/10252886';
 var ENTITY=SERVER+USECASE;
