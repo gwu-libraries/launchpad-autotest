6bef8956dfc0b3290fdc0a5ccba3179ad3cb5a0f
Michael Cummings <michaelc@gwu.edu>
2013-11-15 13:58:08 -0500

    Added note re marc-tests directory and scripts

--- m15-001.sh
+++ m15-001.sh
@@ -4,6 +4,10 @@
 # Each numbered item corresponds to a .js file of that name 
 # (eg., 362.js)
 #
+# A set of test scripts to verify MARC fields are stored in the
+# directory marc-tests along with a copy of the script issues.sh
+# There you will find a script for every MARC field identified in
+# lp/ui/tests/marc_test.py and on the MARC Extraction wiki page.
 sh issue.sh 180
 sh issue.sh 400a
 sh issue.sh 400b
