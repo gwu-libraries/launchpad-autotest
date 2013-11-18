5760d1de88d5a36efce10821010ad4ac77f37112
Michael Cummings <michaelc@gwu.edu>
2013-11-18 17:08:56 -0500

    edited name of called scripts

--- marc-tests/marc-all.sh
+++ marc-tests/marc-all.sh
@@ -1,56 +1,79 @@
 # marc-all.sh
 #
 # 			Standard Title
-sh issue.sh marc-240
+sh marc-test.sh marc-240
 # 			Other Title
-sh issue.sh marc-130
-sh issue.sh marc-242
-sh issue.sh marc-246
-sh issue.sh marc-247
-sh issue.sh marc-730
-sh issue.sh marc-740
+sh marc-test.sh marc-130
+sh marc-test.sh marc-242
+sh marc-test.sh marc-246
+sh marc-test.sh marc-247
+sh marc-test.sh marc-730
+sh marc-test.sh marc-740
 # 			Other Authors
-sh issue.sh marc-700
-sh issue.sh marc-710
-sh issue.sh marc-711
+sh marc-test.sh marc-700
+sh marc-test.sh marc-710
+sh marc-test.sh marc-711
 #			Earlier Title
-sh issue.sh marc-780
+sh marc-test.sh marc-780
 #			Title Changed To
-sh issue.sh marc-785
+sh marc-test.sh marc-785
 #			Copyright Date
-sh issue.sh marc-264
+sh marc-test.sh marc-264
 #
 #			Current Frequency
-sh issue.sh marc-310
-sh issue.sh marc-321
-sh issue.sh marc-362
+sh marc-test.sh marc-310
+sh marc-test.sh marc-321
+sh marc-test.sh marc-362
 #			Series
-sh issue.sh marc-400
-sh issue.sh marc-800
-sh issue.sh marc-810
-sh issue.sh marc-811
-sh issue.sh marc-830
+sh marc-test.sh marc-400
+sh marc-test.sh marc-800
+sh marc-test.sh marc-810
+sh marc-test.sh marc-811
+sh marc-test.sh marc-830
 #			In collection
-sh issue.sh marc-773
+sh marc-test.sh marc-773
 #
 #			Subjects
-sh issue.sh marc-600
-sh issue.sh marc-610
-sh issue.sh marc-630
-sh issue.sh marc-650
-sh issue.sh marc-651
+sh marc-test.sh marc-600
+sh marc-test.sh marc-610
+sh marc-test.sh marc-630
+sh marc-test.sh marc-650
+sh marc-test.sh marc-651
 #			Subject / Genre
-sh issue.sh marc-655
+sh marc-test.sh marc-655
 #
 #			Description 3xx
-sh issue.sh marc-300
-sh issue.sh marc-344
-sh issue.sh marc-346
-sh issue.sh marc-347
+sh marc-test.sh marc-300
+sh marc-test.sh marc-344
+sh marc-test.sh marc-346
+sh marc-test.sh marc-347
 #
 #			Notes 5xx
 #
+sh marc-test.sh marc-500
+sh marc-test.sh marc-502
+sh marc-test.sh marc-504
+sh marc-test.sh marc-505
+sh marc-test.sh marc-507
+sh marc-test.sh marc-508
+sh marc-test.sh marc-510
+sh marc-test.sh marc-511
+sh marc-test.sh marc-516
+sh marc-test.sh marc-520
+sh marc-test.sh marc-530
+sh marc-test.sh marc-533
+sh marc-test.sh marc-534
+sh marc-test.sh marc-538
+sh marc-test.sh marc-540
+sh marc-test.sh marc-546
+sh marc-test.sh marc-547
+sh marc-test.sh marc-550
+sh marc-test.sh marc-555
+sh marc-test.sh marc-580
+sh marc-test.sh marc-586
+sh marc-test.sh marc-588
+#
 #			Description
-sh issue.sh marc-516	
+sh marc-test.sh marc-516	
 #			Funding / Sponsors
-sh issue.sh marc-536
+sh marc-test.sh marc-536
