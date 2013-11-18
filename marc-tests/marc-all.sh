4a2b320ef10cef1ad6f339c44091fa97a86ed561
Michael Cummings <michaelc@gwu.edu>
2013-11-18 09:58:44 -0500

    added Series

--- marc-tests/marc-all.sh
+++ marc-tests/marc-all.sh
@@ -1,29 +1,36 @@
 # marc-all.sh
 #
-# 				Standard Title
-sh marc-test.sh marc-240
-# 				Other Title
-sh marc-test.sh marc-130
-sh marc-test.sh marc-246
-sh marc-test.sh marc-247
-sh marc-test.sh marc-730
-sh marc-test.sh marc-740
-# 				Other Authors
-sh marc-test.sh marc-700
-sh marc-test.sh marc-710
-sh marc-test.sh marc-711
-#				Earlier Title
-sh marc-test.sh marc-780
-#				Title Changed To
-sh marc-test.sh marc-785
-#				Copyright Date
-sh marc-test.sh marc-264
+# 			Standard Title
+sh issue.sh marc-240
+# 			Other Title
+sh issue.sh marc-130
+sh issue.sh marc-242
+sh issue.sh marc-246
+sh issue.sh marc-247
+sh issue.sh marc-730
+sh issue.sh marc-740
+# 			Other Authors
+sh issue.sh marc-700
+sh issue.sh marc-710
+sh issue.sh marc-711
+#			Earlier Title
+sh issue.sh marc-780
+#			Title Changed To
+sh issue.sh marc-785
+#			Copyright Date
+sh issue.sh marc-264
 #
-#				Current Frequency
-sh marc-test.sh marc-310
-sh marc-test.sh marc-321
-sh marc-test.sh marc-362
+#			Current Frequency
+sh issue.sh marc-310
+sh issue.sh marc-321
+sh issue.sh marc-362
+#			Series
+sh issue.sh marc-400
+sh issue.sh marc-800
+sh issue.sh marc-810
+sh issue.sh marc-811
+sh issue.sh marc-830
 #
-#				NOTES
-#				Funding / Sponsors
-sh marc-test.sh marc-536
+#			NOTES
+#			Funding / Sponsors
+sh issue.sh marc-536
