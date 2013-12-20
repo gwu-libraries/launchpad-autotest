b57ee4cd8b7db966d5e17a2ce3505736c286021e
Michael Cummings <michaelc@gwu.edu>
2013-12-20 12:26:50 -0500

    updated test

--- 344.js
+++ 344.js
@@ -1,14 +1,15 @@
 // m14_001 344 
-casper.test.begin('m14_001 344 WRLC Catalog link', 5, function suite(test) {
+// m15 supercedes this issue, removing all tests except title
+casper.test.begin('m14_001 344 WRLC Catalog link', 1, function suite(test) {
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/11880965';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
         test.assertTextExists("Brigham Young", "page with title Brigham Young... found");
-        test.assertTextExists("WRLC Catalog","WRLC catalog link found");
-	test.assertExists('span.catlink','found span catlink');
-	test.assertSelectorHasText('span.catlink', 'View detailed description');
-        test.assertTextExists("WRLC Catalog","WRLC catalog link found");
+        //test.assertTextExists("WRLC Catalog","WRLC catalog link found");
+	//test.assertExists('span.catlink','found span catlink');
+	//test.assertSelectorHasText('span.catlink', 'View detailed description');
+        //test.assertTextExists("WRLC Catalog","WRLC catalog link found");
     });
 
     casper.run(function() {
