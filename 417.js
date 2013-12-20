ca89f5503e3a3fda7e68f5424628e5aeb998095d
Michael Cummings <michaelc@gwu.edu>
2013-12-20 12:00:07 -0500

    m15-002 test

---
+++ 417.js
@@ -0,0 +1,15 @@
+// m15_002 #417 #441
+// 
+//
+casper.test.begin('m15_002 #417 #441 do not show lock on purl.access.gpo.gov', 3, function suite(test) { 
+casper.echo("Test environment: "+SERVER);
+var USECASE='/item/9239000'
+var ENTITY=SERVER+USECASE;
+	casper.start(ENTITY, function() {
+	test.assertTextExists('Defense management' , 'page title Defense management... found');
+	test.assertTextDoesntExist('GT only' , 'Verified label GT only omitted');
+	test.assertTextDoesntExist('GM only' , 'Verified label GM only omitted');
+    }).run(function() {
+        test.done();
+    });
+});
