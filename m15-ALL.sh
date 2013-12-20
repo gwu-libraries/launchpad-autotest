05606a993e3133ac8fa98728dd42a5cf124632f9
Michael Cummings <michaelc@gwu.edu>
2013-12-20 12:09:53 -0500

    first commit

---
+++ m15-ALL.sh
@@ -0,0 +1,48 @@
+# m15-ALL.sh
+# all issues in m15-xxx
+#
+# m15-001.sh 
+# Script runs tests, output to screen plus an xml log for each
+# Target url determined by contents of config.sh
+# Each numbered item corresponds to a .js file of that name 
+# (eg., 362.js)
+#
+#  two of the same test
+sh issue.sh 403
+sh issue.sh 415
+#  three of the same test
+sh issue.sh 180
+sh issue.sh 409
+sh issue.sh 416
+#
+sh issue.sh 413
+# 
+sh issue.sh 400a
+sh issue.sh 400b
+sh issue.sh 15json
+# m15-002.sh 
+# Script runs tests, output to screen plus an xml log for each
+# Target url determined by contents of config.sh
+# Each numbered item corresponds to a .js file of that name 
+# (eg., 362.js)
+#
+# 
+sh issue.sh 355
+sh issue.sh 398
+sh issue.sh 417
+sh issue.sh 418
+sh issue.sh 419
+sh issue.sh 420
+sh issue.sh 421
+sh issue.sh 426
+sh issue.sh 427
+sh issue.sh 428
+sh issue.sh 431
+sh issue.sh 433
+sh issue.sh 434
+sh issue.sh 444
+sh issue.sh 450
+sh issue.sh 454
+sh issue.sh 458
+sh issue.sh 464
+
