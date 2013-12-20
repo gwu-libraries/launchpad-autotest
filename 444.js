7f30a6ac348029133075344c7b6d4f76bf605a86
Michael Cummings <michaelc@gwu.edu>
2013-12-20 11:13:14 -0500

    m15-002 test

---
+++ 444.js
@@ -0,0 +1,16 @@
+// m15_002 #444 #449
+// 
+//
+casper.test.begin('m15_002 #444 #449', 4, function suite(test) { 
+casper.echo("Test environment: "+SERVER);
+var USECASE='/issn/0373-6237';
+var ENTITY=SERVER+USECASE;
+	casper.start(ENTITY, function() {
+	test.assertTextExists('Biblioth' , 'page title Biblioth... found');
+	test.assertTextExists('Details' , 'Verified Details tab');
+	test.assertTextExists('Van Ness Electronic Journals' , 'Verified UDC holding found');
+	test.assertTextExists('1953-8138' , 'Verified related ISSN 1953-8138 found');
+    }).run(function() {
+        test.done();
+    });
+});
