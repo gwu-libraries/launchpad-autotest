2ff6ae20b4849c3336ffa925854a58b6731fb5b3
Michael Cummings <michaelc@gwu.edu>
2013-12-20 12:07:16 -0500

    first commit

---
+++ m15-002.sh
@@ -0,0 +1,26 @@
+# m15-002.sh 
+# Script runs tests, output to screen plus an xml log for each
+# Target url determined by contents of config.sh
+# Each numbered item corresponds to a .js file of that name 
+# (eg., 362.js)
+#
+# 
+sh issue.sh 355
+sh issue.sh 398
+sh issue.sh 417
+sh issue.sh 418
+sh issue.sh 419
+sh issue.sh 420
+sh issue.sh 421
+sh issue.sh 426
+sh issue.sh 427
+sh issue.sh 428
+sh issue.sh 431
+sh issue.sh 433
+sh issue.sh 434
+sh issue.sh 444
+sh issue.sh 450
+sh issue.sh 454
+sh issue.sh 458
+sh issue.sh 464
+
