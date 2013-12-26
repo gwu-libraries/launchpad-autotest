806e6d9ba2babc499bfc4068c7623a21160db13a
Michael Cummings <michaelc@gwu.edu>
2013-12-26 12:11:02 -0500

    first commit

---
+++ m13.sh
@@ -0,0 +1,11 @@
+# m13.sh 
+# Script runs tests, output to screen plus an xml log for each
+# Target url determined by contents of config.sh
+# Each numbered item corresponds to a .js file of that name 
+# (eg., 362.js)
+#
+sh issue.sh 273
+sh issue.sh 279
+sh issue.sh 302
+sh issue.sh 318
+sh issue.sh 322
