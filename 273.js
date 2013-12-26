4c9ade6e26371ad48cc5e184fd0178817b3c1b18
Michael Cummings <michaelc@gwu.edu>
2013-12-26 12:11:56 -0500

    first commit

---
+++ 273.js
@@ -0,0 +1,14 @@
+// m13_001 #273
+//
+casper.test.begin('m13_001 #273 find GM title with no WRLC match', 3, function suite(test) { 
+casper.echo("Test environment: "+SERVER);
+var USECASE='/item/m2001110';
+var ENTITY=SERVER+USECASE;
+	casper.start(ENTITY, function() {
+	test.assertTextExists('Public policy in Asia' , 'page displayed for GM title that has no WRLC match'); 
+	test.assertTextExists('George Mason' , 'page displayed has GM holding'); 
+	test.assertTextExists('Interlibrary Loan' , 'page displayed has Interlibrary loan link'); 
+    }).run(function() {
+        test.done();
+    });
+});
