6f62dcb0ff11ff71004ab4eee7d3f38eadfa15f1
Michael Cummings <michaelc@gwu.edu>
2014-01-02 16:06:22 -0500

    edited test string to match

--- marc-tests/marc-534.js
+++ marc-tests/marc-534.js
@@ -7,7 +7,7 @@ var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
 	test.assertTextExists('Flamenco Live!' , 'page title Flamenco Live! ... found');
 	test.assertTextExists('Original Version' , 'Original Version note TAG 534');
-	test.assertTextExists('Originally published: Flamenco Live!:Cante Gitano Disc One 198.', 'found Original Version note');
+	test.assertTextExists('Cante Gitano Disc One 198.', 'found Original Version note');
     }).run(function() {
         test.done();
     });
