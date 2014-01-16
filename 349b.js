3bf09c3e92f570309d73e8ca3678270c6d103f2a
Michael Cummings <michaelc@gwu.edu>
2014-01-16 17:33:03 -0500

    first commit

---
+++ 349b.js
@@ -0,0 +1,16 @@
+// 
+// m16_001 #349
+//
+casper.test.begin('m16_001 #349 show WRLC due date', 3, function suite(test) {
+casper.echo("Test environment: "+SERVER);
+var USECASE='item/2364479';
+var ENTITY=SERVER+USECASE;
+casper.echo(ENTITY);
+    casper.start(ENTITY, function() {
+    test.assertTextExists('Society of Arts', 'book title verified');
+    test.assertTextDoesntExist('DUE', 'found word "DUE"');
+    test.assertTextExists('Off site', 'Status "Off site" (not DUE) verified');
+    }).run(function() {
+        test.done();
+    });
+});
