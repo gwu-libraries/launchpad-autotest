84ee9f2b968bdcd82f9aefe73e7a227f4758505e
Michael Cummings <michaelc@gwu.edu>
2014-01-16 15:45:41 -0500

    first commit

---
+++ 198.js
@@ -0,0 +1,17 @@
+// m16_001 198 
+//
+casper.test.begin('m16_001 #198 display related isbn', 6, function suite(test) {
+casper.echo("Test environment: "+SERVER);
+var USECASE='/item/6238998';
+var ENTITY=SERVER+USECASE;
+    casper.start(ENTITY, function() {
+	test.assertTextExists('Harry Potter and the half-blood prince', 'book title verified');
+	test.assertTextExists('0439784549', 'ISBN ending 549 found.');
+	test.assertTextExists('5960', 'related ISBN found');
+	test.assertTextExists('6770', 'related ISBN found');
+	test.assertTextExists('1324', 'related ISBN found');
+	test.assertTextExists('5969', 'related ISBN found');
+    }).run(function() {
+        test.done();
+    });
+});
