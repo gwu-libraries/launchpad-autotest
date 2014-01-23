cf09049ed776ed6e60ebd1d078a6d303511d819e
Michael Cummings <michaelc@gwu.edu>
2014-01-23 12:44:44 -0500

    first commit

---
+++ 406.js
@@ -0,0 +1,16 @@
+// m16_001 #406 and 501/502
+//
+casper.test.begin('m16_001 #406 and 501/502 retrieve multiple GT holdings', 4, function suite(test) { 
+casper.echo("Test environment: "+SERVER);
+var USECASE='/item/5059977';
+var ENTITY=SERVER+USECASE;
+casper.echo(ENTITY);
+	casper.start(ENTITY, function() {
+    test.assertTextExists('Losing Mogadishu','title matched');
+    test.assertTextExists('Georgetown','institution matched');
+    test.assertTextExists('Lauinger stacks','location #1 matched');
+    test.assertTextExists('Lauinger Special Collections','location #2 matched');
+    }).run(function() {
+        test.done();
+    });
+});
