e22c831ebea77bc104261cca82997adf834ec38d
Michael Cummings <michaelc@gwu.edu>
2014-01-23 14:05:03 -0500

    first commit

---
+++ 512.js
@@ -0,0 +1,13 @@
+// m16_001 #512 set BIB_ID to original GT value
+//
+casper.test.begin('m16_001 #512', 1, function suite(test) {  
+casper.echo("Test environment: "+SERVER);
+var USECASE='item/b35064353.json';
+var ENTITY=SERVER+USECASE;
+casper.echo(ENTITY);
+	casper.start(ENTITY, function() {
+	test.assertTextExists('b35064353', 'MATCH worked');
+    }).run(function() {
+        test.done();
+    });
+});
