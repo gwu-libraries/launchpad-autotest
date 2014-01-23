43285076e9480dfa089526bd9df1f4d740e61add
Michael Cummings <michaelc@gwu.edu>
2014-01-23 12:39:54 -0500

    first commit

---
+++ 402.js
@@ -0,0 +1,17 @@
+// m16_001 #402 and 496
+//
+casper.test.begin('m16_001 #402 and 496 omit ILL for electronic only', 5, function suite(test) { 
+casper.echo("Test environment: "+SERVER);
+var USECASE='/item/12600000';
+var ENTITY=SERVER+USECASE;
+casper.echo(ENTITY);
+	casper.start(ENTITY, function() {
+    test.assertTextExists('Interview with Robinson','title matched');
+    test.assertTextExists('American','institution matched');
+    test.assertTextExists('Electronic text','location matched');
+    test.assertTextExists('Internet Resources','location matched');
+    test.assertTextDoesntExist('Interlibrary Loan','verified ILL is omitted');
+    }).run(function() {
+        test.done();
+    });
+});
