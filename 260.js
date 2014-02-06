9f8f8990c46f6c6c1f88930a5cce2e8e5d24b4a0
Michael Cummings <michaelc@gwu.edu>
2014-02-06 14:30:36 -0500

    first commit

---
+++ 260.js
@@ -0,0 +1,14 @@
+// m16_001 #260 and 525
+//
+casper.test.begin('m16_001 #260 and 525 feedback form read from header', 2, function suite(test) { 
+//casper.echo("Test environment: "+SERVER);
+casper.echo("Based on change to github header.html");
+var ENTITY="https://github.com/gwu-libraries/launchpad/blob/master/lp/ui/templates/header.html"
+casper.echo(ENTITY);
+	casper.start(ENTITY, function() {
+	test.assertUrlMatch(ENTITY, 'url MATCH worked');
+    test.assertTextExists('FEEDBACK_FORM_URL','found setting in header.html');
+    }).run(function() {
+        test.done();
+    });
+});
