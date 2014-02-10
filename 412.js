5b5a10ad20574e28151533fab011c679b75bd4bd
Michael Cummings <michaelc@gwu.edu>
2014-02-10 16:05:48 -0500

    first commit

---
+++ 412.js
@@ -0,0 +1,10 @@
+// m16_001 #412 / 423, 511
+//
+casper.test.begin('m16_001 #412/423, 511 settings for surveyor or summon', 1, function suite(test) {
+casper.echo("Test environment: github.com");
+    casper.start('https://github.com/gwu-libraries/launchpad/blob/master/lp/lp/local_settings.py.template', function() {
+	test.assertTextExists('EXPLORE_TYPE', 'local settings template contains "EXPLORE_TYPE"');
+    }).run(function() {
+        test.done();
+    });
+});
