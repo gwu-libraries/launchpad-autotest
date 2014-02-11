fbfc7b5fa3bf1291bfe669c5860e8ce72a733669
Michael Cummings <michaelc@gwu.edu>
2014-02-11 15:34:21 -0500

    changed test to check apache/lp

--- 339.js
+++ 339.js
@@ -2,8 +2,8 @@
 //
 casper.test.begin('m14_001 #339 readme updated', 1, function suite(test) {
 casper.echo("Test environment: github.com/gwu-libraries");
-    casper.start('https://github.com/gwu-libraries/launchpad/blob/master/README.md', function() {
-	test.assertTextExists('python2.7', 'page body contains "python2.7"');
+    casper.start('https://github.com/gwu-libraries/launchpad/blob/master/apache/lp', function() {
+	test.assertTextExists('python/2.7', 'apache/lp contains "python/2.7"');
     }).run(function() {
         test.done();
     });
