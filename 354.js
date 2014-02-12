d9a958f3cb56bd138d5414c64c1fed1ac894e1aa
Michael Cummings <michaelc@gwu.edu>
2014-02-12 14:41:09 -0500

    update path

--- 354.js
+++ 354.js
@@ -1,9 +1,9 @@
-// m14_001 339 + 354 
+// m16_001 354
 //
-casper.test.begin('m14_001 #339 + 354 readme updated', 1, function suite(test) {
-casper.echo("Test environment: github.com");
-    casper.start('https://github.com/gwu-libraries/launchpad/blob/master/README.md', function() {
-	test.assertTextExists('python2.7', 'page body contains "python2.7"');
+casper.test.begin('m16_001 #354 readme updated', 1, function suite(test) {
+casper.echo("Test environment: github.com/gwu-libraries");
+    casper.start('https://github.com/gwu-libraries/launchpad/blob/master/apache/lp', function() {
+	test.assertTextExists('python/2.7', 'apache/lp contains "python/2.7"');
     }).run(function() {
         test.done();
     });
