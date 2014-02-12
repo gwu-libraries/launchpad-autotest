df9fc2e7e82839856842386eef5ee6a9e7c193c4
Michael Cummings <michaelc@gwu.edu>
2014-02-12 14:34:18 -0500

    add / before item

--- 512.js
+++ 512.js
@@ -2,7 +2,7 @@
 //
 casper.test.begin('m16_001 #512', 1, function suite(test) {  
 casper.echo("Test environment: "+SERVER);
-var USECASE='item/b35064353.json';
+var USECASE='/item/b35064353.json';
 var ENTITY=SERVER+USECASE;
 casper.echo(ENTITY);
 	casper.start(ENTITY, function() {
