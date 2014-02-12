b1452a974902a7318341105aeb237995d4ebe09c
Michael Cummings <michaelc@gwu.edu>
2014-02-12 14:21:56 -0500

    changed test bib

--- 541.js
+++ 541.js
@@ -2,7 +2,7 @@
 //
 casper.test.begin('m16-001 #541 handle GT bib with no related WRLC record', 1, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
-var USECASE='/item/b12450500';
+var USECASE='/item/b12450509';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
 	test.assertTextExists('Memorial addresses','verified title retrieved.');
