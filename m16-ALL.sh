cfc49d8fcb44a7c82b79861f4976acdbd74c12aa
Michael Cummings <michaelc@gwu.edu>
2014-02-12 14:16:23 -0500

    first commit

---
+++ m16-ALL.sh
@@ -0,0 +1,35 @@
+# m16-ALL.sh
+# all issues in m16-xxx
+#
+echo 'Starting m16-001 tests'
+#
+sh issue.sh 198
+sh issue.sh 227
+sh issue.sh 249
+sh issue.sh 260
+sh issue.sh 349
+sh issue.sh 349b
+sh issue.sh 356
+sh issue.sh 375
+sh issue.sh 381
+sh issue.sh 386
+sh issue.sh 392
+sh issue.sh 399
+sh issue.sh 402
+sh issue.sh 406
+sh issue.sh 407
+sh issue.sh 410
+sh issue.sh 412
+sh issue.sh 429
+sh issue.sh 435
+sh issue.sh 473
+sh issue.sh 495
+sh issue.sh 508
+sh issue.sh 512
+sh issue.sh 516
+sh issue.sh 518
+sh issue.sh 536
+sh issue.sh 538
+sh issue.sh 541
+# 
+echo 'Finished m16-ALL tests'
