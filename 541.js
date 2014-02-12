20875afccdeff2d2db754b9c0a6c5b531ab6aee0
Michael Cummings <michaelc@gwu.edu>
2014-02-12 10:01:27 -0500

    first commit

---
+++ 541.js
@@ -0,0 +1,12 @@
+// m16_001 #541
+//
+casper.test.begin('m16-001 #541 handle GT bib with no related WRLC record', 1, function suite(test) { 
+casper.echo("Test environment: "+SERVER);
+var USECASE='/item/b12450500';
+var ENTITY=SERVER+USECASE;
+	casper.start(ENTITY, function() {
+	test.assertTextExists('Memorial addresses','verified title retrieved.');
+    }).run(function() {
+        test.done();
+    });
+});
