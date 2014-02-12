bcb0c2af95f7532365670f9b595e7126c6fb5a7b
Michael Cummings <michaelc@gwu.edu>
2014-02-12 14:44:00 -0500

    removed one test

--- 402.js
+++ 402.js
@@ -1,6 +1,6 @@
 // m16_001 #402 and 496
 //
-casper.test.begin('m16_001 #402 and 496 omit ILL for electronic only', 5, function suite(test) { 
+casper.test.begin('m16_001 #402 and 496 omit ILL for electronic only', 4, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/12600000';
 var ENTITY=SERVER+USECASE;
@@ -8,7 +8,6 @@ casper.echo(ENTITY);
 	casper.start(ENTITY, function() {
     test.assertTextExists('Interview with Robinson','title matched');
     test.assertTextExists('American','institution matched');
-    test.assertTextExists('Electronic text','location matched');
     test.assertTextExists('Internet Resources','location matched');
     test.assertTextDoesntExist('Interlibrary Loan','verified ILL is omitted');
     }).run(function() {
