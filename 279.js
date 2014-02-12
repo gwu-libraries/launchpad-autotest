efb441e5a3575e1e7c92ac6198324994c77aad75
Michael Cummings <michaelc@gwu.edu>
2014-02-12 12:46:08 -0500

    adjusted for m16

--- 279.js
+++ 279.js
@@ -1,13 +1,13 @@
 // m13_001 #279
 //
-casper.test.begin('m13_001 #279 find GT title with no WRLC match', 3, function suite(test) { 
+casper.test.begin('m13_001 #279 find GT title with no WRLC match', 2, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
-var USECASE='/item/b12450500';
+var USECASE='/item/b12450509';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
 	test.assertTextExists('Memorial addresses on the life' , 'page displayed for GT title that has no WRLC match'); 
 	test.assertTextExists('Georgetown' , 'page displayed has GT holding'); 
-	test.assertTextExists('Interlibrary Loan' , 'page displayed has Interlibrary loan link'); 
+	//test.assertTextExists('Interlibrary Loan' , 'page displayed has Interlibrary loan link'); 
     }).run(function() {
         test.done();
     });
