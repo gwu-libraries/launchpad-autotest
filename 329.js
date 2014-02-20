118f182c8d4c33fdf2c98af9da609f6879a4bb47
Michael Cummings <michaelc@gwu.edu>
2014-02-20 10:46:13 -0500

    revised test

--- 329.js
+++ 329.js
@@ -1,11 +1,11 @@
 // m14_001 #329 
 //
-casper.test.begin('m14_001 #329 no GT holding for HU id', 1, function suite(test) { 
+casper.test.begin('m14_001 #329 no GT holding online', 1, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/10252886';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
-	test.assertTextDoesntExist('Georgetown' , 'page body does not contain "GT item"');
+	test.assertTextDoesntExist('Online' , 'page body does not contain "Online"');
     }).run(function() {
         test.done();
     });
