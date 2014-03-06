0d6dd6c86f292972c9773274552a3eca34bf5a0b
Michael Cummings <michaelc@gwu.edu>
2014-03-06 12:45:00 -0500

    changed bibid to test

--- 574.js
+++ 574.js
@@ -3,7 +3,7 @@
 //
 casper.test.begin('m16_003 #574 verify subject separator (after 553)', 2, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
-var USECASE='/item/13460814';
+var USECASE='/item/329034';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
 	test.assertTextExists('The West Indies; a history' , 'page title West Indies... found');
