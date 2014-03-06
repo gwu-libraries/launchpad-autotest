4442ec9dd2648b91ed7d83b142e8c1d5253bdf65
Michael Cummings <michaelc@gwu.edu>
2014-03-06 09:54:30 -0500

    first commit

---
+++ 574.js
@@ -0,0 +1,14 @@
+// m16_003 #574, 578, 553
+// 
+//
+casper.test.begin('m16_003 #574 verify subject separator (after 553)', 2, function suite(test) { 
+casper.echo("Test environment: "+SERVER);
+var USECASE='/item/13460814';
+var ENTITY=SERVER+USECASE;
+	casper.start(ENTITY, function() {
+	test.assertTextExists('The West Indies; a history' , 'page title West Indies... found');
+	test.assertTextExists('West Indies -- Description' , 'Verified subject separator -- found');
+    }).run(function() {
+        test.done();
+    });
+});
