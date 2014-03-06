c1b494429a79eae3c7a7f4e177914ae65b1f1185
Michael Cummings <michaelc@gwu.edu>
2014-03-06 13:48:03 -0500

    first commit, placeholder

---
+++ m16-002.sh
@@ -0,0 +1,14 @@
+# m16-002.sh
+#
+echo 'Starting m16-002 tests'
+#
+# need to write tests for these
+#
+# sh issue.sh 547
+# sh issue.sh 548
+#
+# test 549 requires a new issue to be done, add LAW LIBRARY - 3RD FLOOR to 
+# local_settings.py.template because adding LAW LIBRARY wasn't enough
+# sh issue.sh 549
+# 
+echo 'Finished m16-002 tests'
