204db17247cd53360a617508db169c79b75a7d32
Michael Cummings <michaelc@gwu.edu>
2014-03-10 10:52:56 -0400

    revised test

--- 435.js
+++ 435.js
@@ -1,15 +1,14 @@
-// m16_001 #435 #532
-// 
+// m16_001 #435
+// previous test for issn value was invalid. 
 //
-casper.test.begin('m16_001 #435 #532 Dont show tabs for non wrlc result', 4, function suite(test) { 
+casper.test.begin('m16_001 #435 WRLC shared ebook', 3, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
-var USECASE='/issn/0001-0329';
+var USECASE='/isbn/9781452220987';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
-	test.assertTextExists('procedures for helping' , 'page title Home visiting... found');
-	test.assertTextDoesntExist('Details' , 'Verified Details tab removed');
-	test.assertTextDoesntExist('Availability' , 'Verified Availability tab removed');
-	test.assertTextExists('Interlibrary Loan', 'Verified ILL link provided');
+	test.assertTextExists('Home visiting' , 'page Home visiting... found by ISBN query');
+	test.assertTextExists('Details' , 'Verified Details label');
+	test.assertTextExists('Electronic books' , 'Verified content of Genre');
     }).run(function() {
         test.done();
     });
