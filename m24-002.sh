d42d08824c1d2568a571c31d6bf6257bad9e6a0e
Michael Cummings <michaelc@gwu.edu>
2014-09-23 11:12:22 -0400

    first commit

---
+++ m24-002.sh
@@ -0,0 +1,12 @@
+# m24-002.sh
+#
+echo 'Starting m24-002 tests (includes from m16-003 to m24-002'
+#
+sh issue.sh 679
+sh issue.sh 809
+sh issue.sh 810
+sh issue.sh 829
+sh issue.sh 836
+sh issue.sh 843
+# 
+echo 'Finished m24-002 tests'
