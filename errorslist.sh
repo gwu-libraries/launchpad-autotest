517d62506ce480235f3d7283068161255ee141b5
Michael Cummings <michaelc@gwu.edu>
2014-09-23 10:24:13 -0400

    added message for no errors

--- errorslist.sh
+++ errorslist.sh
@@ -1,4 +1,12 @@
 #
 # this command will identify any log file that has failures
 #
-grep -v "failures=\"0\"" *.xml | cut -f1 -d:
+NERR=`ls | grep -c xml`
+if [ $NERR -ne 0 ]; 
+then {
+    grep -v "failures=\"0\"" *.xml | cut -f1 -d: 2>/tmp/junk.out
+    }
+else {
+    echo "No errors found."
+}
+fi
