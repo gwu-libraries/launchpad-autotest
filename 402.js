7a302dc06f8db8a6b93a42041dbe7bd83bc16eee
Michael Cummings <michaelc@gwu.edu>
2014-09-23 10:10:59 -0400

    changed test item

--- 402.js
+++ 402.js
@@ -1,8 +1,9 @@
 // m16_001 #402 and 496
+// 
 //
 casper.test.begin('m16_001 #402 and 496 omit ILL for electronic only', 4, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
-var USECASE='/item/12600000';
+var USECASE='/item/11509414';
 var ENTITY=SERVER+USECASE;
 casper.echo(ENTITY);
 	casper.start(ENTITY, function() {
