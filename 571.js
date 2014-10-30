083068ce717e881f0bb87bf87c47d32f456c9b20
Michael Cummings <michaelc@gwu.edu>
2014-10-30 10:06:25 -0400

    comment out github page

--- 571.js
+++ 571.js
@@ -1,13 +1,14 @@
 // 
 // m16_003 #571
 //
-casper.test.begin('m16_003 #571 left border', 1, function suite(test) {
-casper.echo("Test environment: git repository ");
-var ENTITY='https://github.com/gwu-libraries/launchpad/blob/master/lp/ui/templates/about.html'
-casper.echo(ENTITY);
-    casper.start(ENTITY, function() {
-    test.assertTextExists('.5em;', 'content');
-    }).run(function() {
-        test.done();
-    });
-});
+//casper.test.begin('m16_003 #571 left border', 1, function suite(test) {
+//casper.echo("Test environment: git repository ");
+//var ENTITY='https://github.com/gwu-libraries/launchpad/blob/master/lp/ui/templates/about.html'
+//casper.echo(ENTITY);
+    //casper.start(ENTITY, function() {
+    //test.assertTextExists('0 .5em;');
+    //}).run(function() {
+        //test.done();
+    //});
+//});
+// cant read the github page
