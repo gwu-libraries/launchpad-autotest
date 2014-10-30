f426902eca0890c3b6cf91c63a808d87eeb0fa53
Michael Cummings <michaelc@gwu.edu>
2014-10-30 10:07:47 -0400

    cannot read github page

--- 546.js
+++ 546.js
@@ -1,13 +1,14 @@
 // 
 // m16_003 #546
 //
-casper.test.begin('m16_003 #546 left border', 1, function suite(test) {
-casper.echo("Test environment: git repository ");
-var ENTITY='https://github.com/gwu-libraries/launchpad/blob/master/lp/ui/templates/api.html'
-casper.echo(ENTITY);
-    casper.start(ENTITY, function() {
-    test.assertTextExists('.5em;', 'content');
-    }).run(function() {
-        test.done();
-    });
-});
+//casper.test.begin('m16_003 #546 left border', 1, function suite(test) {
+//casper.echo("Test environment: git repository ");
+//var ENTITY='https://github.com/gwu-libraries/launchpad/blob/master/lp/ui/templates/api.html'
+//casper.echo(ENTITY);
+    //casper.start(ENTITY, function() {
+    //test.assertTextExists('.5em;', 'content');
+    //}).run(function() {
+        //test.done();
+    //});
+//});
+// can not read github page
