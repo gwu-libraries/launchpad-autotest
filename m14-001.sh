ed3fe643e7b0c902209c0e6b3691df2e143a10c3
Michael Cummings <michaelc@gwu.edu>
2014-10-30 10:54:59 -0400

    remove github issues

--- m14-001.sh
+++ m14-001.sh
@@ -5,12 +5,12 @@
 # (eg., 362.js)
 #
 sh issue.sh 362
-sh issue.sh 354
+#sh issue.sh 354
 sh issue.sh 353
 sh issue.sh 350
 sh issue.sh 344
 sh issue.sh 348
-sh issue.sh 339
+#sh issue.sh 339
 sh issue.sh 337
 sh issue.sh 329
 sh issue.sh 328
