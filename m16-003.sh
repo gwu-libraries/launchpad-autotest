59e2c783d22459141ea48f663b8023b0d399e390
Michael Cummings <michaelc@gwu.edu>
2014-10-30 10:55:10 -0400

    remove github issues

--- m16-003.sh
+++ m16-003.sh
@@ -3,13 +3,13 @@
 echo 'Starting m16-003 tests'
 #
 sh issue.sh 488
-sh issue.sh 546
+#sh issue.sh 546
 sh issue.sh 553
 sh issue.sh 555
 sh issue.sh 556
 sh issue.sh 558
 sh issue.sh 563
-sh issue.sh 571
+#sh issue.sh 571
 sh issue.sh 574
 # 
 echo 'Finished m16-003 tests'
