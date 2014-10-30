9f7153fb7dfe244c4c0b89f821a55d08677f9c5b
Michael Cummings <michaelc@gwu.edu>
2014-10-30 10:19:26 -0400

    can not read github page

--- 354.js
+++ 354.js
@@ -1,10 +1,11 @@
 // m16_001 354
 //
-casper.test.begin('m16_001 #354 readme updated', 1, function suite(test) {
-casper.echo("Test environment: github.com/gwu-libraries");
-    casper.start('https://github.com/gwu-libraries/launchpad/blob/master/apache/lp', function() {
-	test.assertTextExists('python/2.7', 'apache/lp contains "python/2.7"');
-    }).run(function() {
-        test.done();
-    });
-});
+//casper.test.begin('m16_001 #354 readme updated', 1, function suite(test) {
+//casper.echo("Test environment: github.com/gwu-libraries");
+    //casper.start('https://github.com/gwu-libraries/launchpad/blob/master/apache/lp', function() {
+	//test.assertTextExists('python/2.7', 'apache/lp contains "python/2.7"');
+    //}).run(function() {
+        //test.done();
+    //});
+//});
+// can not read github page
