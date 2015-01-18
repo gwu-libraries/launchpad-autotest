eb08c895fe363dc8f0a3e11d215ce07e3a82f8e5
Michael Cummings <michaelc@gwu.edu>
2015-01-18 12:28:12 -0500

    update test case to include audio and video

--- 809.js
+++ 809.js
@@ -2,7 +2,7 @@
 //
 casper.test.begin('m23_002 #809 audio video labels', 4, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
-var USECASE='/search?q=2001';
+var USECASE='/search?q=west side story';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
 	test.assertTextDoesntExist('Audio Recording' , '');
