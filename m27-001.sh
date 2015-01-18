59856a0a4aae84ce511fbd098427d626e4b1cdd2
Michael Cummings <michaelc@gwu.edu>
2015-01-18 13:20:26 -0500

    first commit

---
+++ m27-001.sh
@@ -0,0 +1,13 @@
+# m27_001.sh
+# not included 931, 867, 828, 929, 923
+# add 923 after some DDA titles are available
+echo 'Starting m27-001 tests '
+#
+sh issue.sh 597
+sh issue.sh 640
+sh issue.sh 795
+sh issue.sh 924
+sh issue.sh 936
+sh issue.sh 937
+# 
+echo 'Finished m27-001 tests'
