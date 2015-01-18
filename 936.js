e6bda6822c53255b1091e5d93c454c6a324f3653
Michael Cummings <michaelc@gwu.edu>
2015-01-18 13:10:39 -0500

    first commit. No lock non-Gw open eresource

---
+++ 936.js
@@ -0,0 +1,14 @@
+// m27_001 #936 
+//
+casper.test.begin('m27_001 #936 some non-GW available', 4, function suite(test) { 
+casper.echo("Test environment: "+SERVER);
+var USECASE='/item/12643113';
+var ENTITY=SERVER+USECASE;
+	casper.start(ENTITY, function() {
+	test.assertTextExists('Georgetown' , 'found Georgetown holding');
+	test.assertTextExists('INTERNET' , 'found INTERNET');
+	test.assertTextExists('Available to all' , 'found Available to all message'); test.assertTextExists('Art of the Islamic world' , 'matched title');
+    }).run(function() {
+        test.done();
+    });
+});
