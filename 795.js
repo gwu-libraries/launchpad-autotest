9bfac743a603c28dd53cf0c4e303853ea09c7719
Michael Cummings <michaelc@gwu.edu>
2015-01-18 12:45:50 -0500

    updated test message

--- 795.js
+++ 795.js
@@ -5,9 +5,9 @@ casper.echo("Test environment: "+SERVER);
 var USECASE='/item/11494185';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
-	test.assertTextExists('Heidegger' , '');
-	test.assertTextExists('Gelman stacks' , '');
-	test.assertTextExists('Missing' , '');
+	test.assertTextExists('Heidegger' , 'matched title');
+	test.assertTextExists('Gelman stacks' , 'matched location');
+	test.assertTextExists('Missing' , 'found status Missing');
     }).run(function() {
         test.done();
     });
