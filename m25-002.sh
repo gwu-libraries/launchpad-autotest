2c8bcf5d76f43869a14a928484d19feb9346311b
Michael Cummings <michaelc@gwu.edu>
2015-01-21 16:27:38 -0500

    first commit

---
+++ m25-002.sh
@@ -0,0 +1,13 @@
+# m25_002.sh
+# not included 791, 882, 903
+echo 'Starting m25-001 tests '
+#
+sh issue.sh 596
+sh issue.sh 734
+sh issue.sh 777a
+sh issue.sh 777v
+sh issue.sh 777s
+sh issue.sh 777m
+sh issue.sh 777d
+# 
+echo 'Finished m25-002 tests'
