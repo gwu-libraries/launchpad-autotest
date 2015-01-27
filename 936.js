976e1a32c6d22e83a3359af02f38aea22069dc95
Michael Cummings <michaelc@gwu.edu>
2015-01-27 11:38:18 -0500

    updated matching criteria per issue 947

--- 936.js
+++ 936.js
@@ -6,7 +6,7 @@ var USECASE='/item/12643113';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
 	test.assertTextExists('Georgetown' , 'found Georgetown holding');
-	test.assertTextExists('INTERNET' , 'found INTERNET');
+	test.assertTextExists('Online' , 'found Online holding');
 	test.assertTextExists('Available to all' , 'found Available to all message'); test.assertTextExists('Art of the Islamic world' , 'matched title');
     }).run(function() {
         test.done();
