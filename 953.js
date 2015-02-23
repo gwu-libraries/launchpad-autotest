ad36fa27807e8183f8673106b9920034f639006b
Michael Cummings <michaelc@gwu.edu>
2015-02-23 16:57:51 -0500

    first commit. added subject fields to advanced search

---
+++ 953.js
@@ -0,0 +1,17 @@
+// m29_001 #953 added fields to advanced search
+// 
+// 
+casper.test.begin('m29_001 #953 added fields to advanced search', 5, function suite(test) { 
+casper.echo("Test environment: "+SERVER);
+var USECASE='/advanced/';
+var ENTITY=SERVER+USECASE;
+	casper.start(ENTITY, function() {
+	test.assertTextExists('Subject Heading Group' , 'matched subject heading group');
+	test.assertTextExists('Region' , 'found region');
+	test.assertTextExists('Time Period' , 'found time period');
+	test.assertTextExists('Genre' , 'found genre');
+	test.assertTextExists('Publishing Group' , 'found publishing group');
+    }).run(function() {
+        test.done();
+    });
+});
