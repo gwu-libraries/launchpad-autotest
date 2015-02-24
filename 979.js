fad58e47db84c2534e0977444545a7e1218892a1
Michael Cummings <michaelc@gwu.edu>
2015-02-24 10:22:24 -0500

    first commit. test revised label

---
+++ 979.js
@@ -0,0 +1,14 @@
+// m29_001 #979 changed label to say Export citation to:
+// 
+// 
+casper.test.begin('m29_001 #979 Export citation to', 3, function suite(test) { 
+casper.echo("Test environment: "+SERVER);
+var USECASE='/item/1234567';
+var ENTITY=SERVER+USECASE;
+	casper.start(ENTITY, function() {
+	test.assertTextExists('The history of broadcasting' , 'matched title');
+	test.assertTextExists('Export citation' , 'verified Export citation label');
+    }).run(function() {
+        test.done();
+    });
+});
