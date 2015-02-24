a6ac000d2a4646c296c86393f5f4060987be2278
Michael Cummings <michaelc@gwu.edu>
2015-02-24 10:17:11 -0500

    first commit. m29 tests

---
+++ m29-001.sh
@@ -0,0 +1,20 @@
+# m29_001.sh
+# not included 
+# 
+echo 'Starting m29-001 tests '
+# bug fix
+sh issue.sh 664
+# bug fix
+sh issue.sh 798
+# bug fix
+sh issue.sh 854
+#
+sh issue.sh 952
+sh issue.sh 953
+sh issue.sh 959
+sh issue.sh 968
+sh issue.sh 979
+# bug fix
+sh issue.sh 977
+# 
+echo 'Finished m29-001 tests'
