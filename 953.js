50237688ed19f767a6887b31f5d1afc393c32120
Michael Cummings <michaelc@gwu.edu>
2015-03-16 17:37:31 -0400

    updated for m29_001

--- 953.js
+++ 953.js
@@ -1,7 +1,7 @@
 // m29_001 #953 added fields to advanced search
 // 
 // 
-casper.test.begin('m29_001 #953 added fields to advanced search', 5, function suite(test) { 
+casper.test.begin('m29_001 #953 added fields to advanced search', 4, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/advanced/';
 var ENTITY=SERVER+USECASE;
@@ -10,7 +10,6 @@ var ENTITY=SERVER+USECASE;
 	test.assertTextExists('Region' , 'found region');
 	test.assertTextExists('Time Period' , 'found time period');
 	test.assertTextExists('Genre' , 'found genre');
-	test.assertTextExists('Publishing Group' , 'found publishing group');
     }).run(function() {
         test.done();
     });
