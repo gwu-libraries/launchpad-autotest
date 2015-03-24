4ed9fcd1326eace33dae52afc0e4d6a2d84aa73a
Michael Cummings <michaelc@gwu.edu>
2015-03-24 14:24:34 -0400

    first commit. m29-003 & 002

---
+++ m29-003.sh
@@ -0,0 +1,18 @@
+# m29_003.sh and m29-002
+# not included 950, 951, 975, 962, 956, 967
+# 
+echo 'Starting m29-002 tests '
+sh issue.sh 946
+sh issue.sh 984
+sh issue.sh 985
+sh issue.sh 988
+# 
+echo 'Finished m29-002 tests'
+#
+#
+echo 'Starting m29-003 tests'
+sh issue.sh 915
+sh issue.sh 997
+sh issue.sh 999
+
+
