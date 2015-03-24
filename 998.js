7745dc33affe3309e81cd895c29b9f26bd8dfcab
Michael Cummings <michaelc@gwu.edu>
2015-03-24 11:29:48 -0400

    update

--- 998.js
+++ 998.js
@@ -1,7 +1,7 @@
-// m29_003 #998 Georgetown Law (985 did not get applied to m29_002)
+// m29_002 #998 Georgetown Law
 // 
 // 
-casper.test.begin('m29_003 #998 Georgetown Law', 2, function suite(test) { 
+casper.test.begin('m29_002 #998 Georgetown Law', 2, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/6308419';
 var ENTITY=SERVER+USECASE;
