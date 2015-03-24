f380b83f935f4695c52b53ee88deca87edb7aaf9
Michael Cummings <michaelc@gwu.edu>
2015-03-24 11:05:11 -0400

    first commit. verify GTL holding

---
+++ 985.js
@@ -0,0 +1,14 @@
+// m29_002 #985 Georgetown Law
+// 
+// 
+casper.test.begin('m29_002 #985 Georgetown Law', 2, function suite(test) { 
+casper.echo("Test environment: "+SERVER);
+var USECASE='/item/6308419';
+var ENTITY=SERVER+USECASE;
+	casper.start(ENTITY, function() {
+	test.assertTextExists('Akhil Reed Amar' , 'matched author');
+	test.assertTextExists('Georgetown Law' , 'matched Georgetown Law');
+    }).run(function() {
+        test.done();
+    });
+});
