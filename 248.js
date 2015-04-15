522d252e8f21c4e63808f01f401c49aad127d533
Dan Chudnov <dchud@umich.edu>
2015-04-15 16:31:12 -0400

    changed "GWU" to "GW" in page title

--- 248.js
+++ 248.js
@@ -5,7 +5,7 @@ casper.echo("Test environment: "+SERVER);
 var USECASE='/item/823422';
 var ENTITY=SERVER+USECASE;
     casper.start(ENTITY, function() {
-        test.assertTitle('Love of the Sacred Heart. - GWU Libraries','Title 823422 verified.');
+        test.assertTitle('Love of the Sacred Heart. - GW Libraries','Title 823422 verified.');
 	test.assertTextExists('Catholic', 'page body contains "Catholic"');
 	test.assertTextExists('BT 2591 .D69', 'page body contains "BT 2591 .D69"');
 	test.assertTextExists('Georgetown', 'page body contains "Georgetown"');
