4a66c12358d79149c0a4a0912490cceb19b9a297
Michael Cummings <michaelc@gwu.edu>
2015-07-06 11:46:08 -0400

    record removed from voyager, test void

--- 371.js
+++ 371.js
@@ -1,11 +1,11 @@
 // m14_003 371 + 372 
-//
+// record has been deleted.
 casper.test.begin('m14_003 #371+372 ISBN diacritic does not trigger error', 1, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/12280154';
 var ENTITY=SERVER+USECASE;
-	casper.start(ENTITY, function() {
-	test.assertTextExists('qui comptent', 'page loaded without throwing error');
+casper.start(ENTITY, function() {
+	test.assertTextDoesntExist('qui comptent', 'page not found');
     }).run(function() {
         test.done();
     });
