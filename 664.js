f7fa092a816a2f7b048bf24ed9d720a00458e84f
Michael Cummings <michaelc@gwu.edu>
2015-07-06 12:00:13 -0400

    edited to match different item which is now on reserve at GM

--- 664.js
+++ 664.js
@@ -1,13 +1,14 @@
 // m29_001 #664 show GM reserves (temporary holding location)
+// Note this test will fail if/when the item is taken off reserver at GM
 //
 // 
 casper.test.begin('m29_001 #664 show GM reserves (temporary holding location)', 2, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
-var USECASE='/item/14214024';
+var USECASE='/item/12703930';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
-	test.assertTextExists('All she was worth' , 'matched title');
-	test.assertTextExists('2 Hour Reserve' , 'found Reserve holding');
+	test.assertTextExists('Institutional economics' , 'matched title');
+	test.assertTextExists('2 Hour' , 'found Reserve holding');
     }).run(function() {
         test.done();
     });
