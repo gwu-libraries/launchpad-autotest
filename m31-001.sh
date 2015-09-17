c6851bc24befffd3c44009cbc10ddb307530c1f7
Michael Cummings <michaelc@gwu.edu>
2015-09-17 11:38:57 -0400

    first commit m31-001.sh

---
+++ m31-001.sh
@@ -0,0 +1,8 @@
+# m31_001.sh
+# not included 1006 search button on top of advanced search form
+# 
+echo 'Starting m31-001 tests '
+sh issue.sh 1007
+sh issue.sh 1008
+sh issue.sh 1010
+echo 'Finished m31-001 tests'
