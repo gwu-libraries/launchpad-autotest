22f8b5a6d51b1a918fe6df517ad10a422539dc9c
Daniel Chudnov <dchud@gwu.edu>
2015-09-18 15:47:33 -0400

    updated title, changed holdings count

--- 356.js
+++ 356.js
@@ -1,14 +1,14 @@
 // m16_001 #356
-// There will be total of 12 items when this is working properly
+// There will be total of 11 items when this is working properly
 // 
 casper.test.begin('m16_001 #356 list UDC items for specific holding', 3, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/1351475';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
-	test.assertTextExists('Mencken, by Philip Wagner','verified title'); 
+	test.assertTextExists('Mencken / by Philip Wagner','verified title'); 
 	test.assertTextExists('Van Ness stacks' , 'page includes Van Ness holdings'); 
-	test.assertElementCount('div.itemrow',12,'found total of 12 items'); 
+	test.assertElementCount('div.itemrow', 11, 'found total of 11 items'); 
     }).run(function() {
         test.done();
     });
