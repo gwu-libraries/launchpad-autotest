07bf1e922704b04d631d7c76606b1e05e5f132db
Michael Cummings <michaelc@gwu.edu>
2015-09-21 13:16:21 -0400

    update note

--- m32-001.sh
+++ m32-001.sh
@@ -3,6 +3,7 @@
 # 869 reduce cache time (this is done on the local_settings.py file)
 #1005 update Django to a later version 
 #1017 (no use case, construction ended?)
+#1030 fixed key error in search result
 echo 'Starting m32-001 tests '
 sh issue.sh 1020
 sh issue.sh 1021
