b6437536e643f11748cb63979993c5fea965f1bd
Michael Cummings <michaelc@gwu.edu>
2015-09-21 10:21:59 -0400

    first commit

---
+++ m32-001.sh
@@ -0,0 +1,10 @@
+# m31_001.sh
+# not included 
+# 869 reduce cache time (this is done on the local_settings.py file)
+#1005 update Django to a later version 
+#1017 (no use case, construction ended?)
+echo 'Starting m32-001 tests '
+sh issue.sh 1020
+sh issue.sh 1021
+sh issue.sh 1027
+echo 'Finished m32-001 tests'
