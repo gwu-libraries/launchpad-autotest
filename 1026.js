4ffd667bc1ae60e19a2b8a9460051878c97f398e
Laura Wrubel <lwrubel@gwu.edu>
2015-10-12 13:05:41 -0400

    cleaning up comments

--- 1026.js
+++ 1026.js
@@ -1,14 +1,11 @@
-// This is a template for creating a test script. 
-// Replace {NAME} with an appropriate string.
+// m33_001 list only a few subject headings and toggle to view more
 //
-// m{MILESTONE} {ISSUE NUMBER(s)}
-// note: the parameter before functon suite(test) is the number of tests to perform
 casper.test.begin('m33_001 #1026 subject heading list toggle', 6, function suite(test) {
 casper.echo("Test environment: "+SERVER);
 var USECASE='/search?q=Statistics+and+reality+%3A+concepts+and+measurements+of+migration+in+Europe';
 var ENTITY=SERVER+USECASE;
     casper.start(ENTITY, function() {
-        //verify the record is showing
+        // verify the record is showing
         test.assertTextExists('Fassman','author Fassman appearing');	
         // verify that collapsed div is present
         test.assertExists('a[href="#moresubjects7703715"]','a:href with url moresubjects7703715 exists');
