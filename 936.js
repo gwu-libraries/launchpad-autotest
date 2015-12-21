2441c51803029518bb5a825737ceae1beac22e96
Michael Cummings <michaelc@gwu.edu>
2015-12-21 12:17:39 -0500

    fixed test count =2

--- 936.js
+++ 936.js
@@ -1,6 +1,6 @@
 // m27_001 #936 
 //
-casper.test.begin('m27_001 #936 some non-GW available', 3, function suite(test) { 
+casper.test.begin('m27_001 #936 some non-GW available', 2, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/12643113';
 var ENTITY=SERVER+USECASE;
