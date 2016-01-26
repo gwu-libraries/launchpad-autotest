2e560f4940d21870ce50cc184564ff9d04007b19
Michael Cummings <michaelc@gwu.edu>
2016-01-26 11:26:56 -0500

    update use case

--- 1021.js
+++ 1021.js
@@ -3,10 +3,10 @@
 // 
 casper.test.begin('m31_001 #1021 JSTOR DDA not eligible for CLS request', 3, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
-var USECASE='/item/15090040';
+var USECASE='/item/15663185';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
-	test.assertTextExists('0691115311' , 'verified page');
+	test.assertTextExists('Instrumental community' , 'verified title');
 	test.assertTextExists('JSTOR DDA' , 'verified DDA holding');
 	test.assertTextDoesntExist('Request' , 'verified no request link on page');
     }).run(function() {
