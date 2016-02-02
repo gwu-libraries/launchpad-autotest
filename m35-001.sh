3e049e292f2b4a1ea676a6609f53b746878dd747
Michael Cummings <michaelc@gwu.edu>
2016-02-02 14:20:53 -0500

    first commit

---
+++ m35-001.sh
@@ -0,0 +1,19 @@
+# m35_001.sh
+# not included  
+# GW Museum no CLS 1087 
+# HathiTrust link with '$'sign in url 1096 
+# Color of hide/show link 1088
+# 
+echo 'Starting m35-001 tests '
+sh issue.sh 1019
+sh issue.sh 1024
+sh issue.sh 1057
+sh issue.sh 1061
+sh issue.sh 1063
+sh issue.sh 1066
+sh issue.sh 1072
+sh issue.sh 1078
+sh issue.sh 1084
+sh issue.sh 1085
+sh issue.sh 1100
+echo 'Finished m35-001 tests'
