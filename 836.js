196ca346382845dc92f0acd282e95ae73b31e533
Michael Cummings <michaelc@gwu.edu>
2016-02-04 15:57:21 -0500

    update use case, GT no longer holding

--- 836.js
+++ 836.js
@@ -1,6 +1,6 @@
 // m23_002 #836
 //
-casper.test.begin('m23_002 #836 title match does not eliminate match', 5, function suite(test) { 
+casper.test.begin('m23_002 #836 title match does not eliminate match', 4, function suite(test) { 
 // voyager.py tests first 8 characters. So this title and the bib with the title
 // Miracles : [electronic resource] ... are still matched
 casper.echo("Test environment: "+SERVER);
@@ -10,7 +10,6 @@ var ENTITY=SERVER+USECASE;
 	test.assertTextExists('Miracles : wonder and meaning' , 'Verified title');
 	test.assertTextExists('Shared E-Resources' , 'Verified WRLC E-Resource holding present');
 	test.assertTextExists('George Mason' , 'Verified GMU holding present');
-	test.assertTextExists('Georgetown' , 'Verified GT holding present');
 	test.assertTextExists('Marymount' , 'Verified Marymount holding present');
     }).run(function() {
         test.done();
