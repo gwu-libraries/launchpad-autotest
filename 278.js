0b0e02b2dbdb40f96b64721bd8ed631aeaa8482b
Michael Cummings <michaelc@gwu.edu>
2016-02-04 15:31:14 -0500

    update use case deleted from WRLC

--- 278.js
+++ 278.js
@@ -1,14 +1,11 @@
 // m14_004 278 
 //
-casper.test.begin('m14_004 #278 page loads with diacritics', 3, function suite(test) {
+casper.test.begin('m14_004 #278 page loads with diacritics', 1, function suite(test) {
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/7765894';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
-	test.info("If this works, the JSON output should be alright");
- 	test.assertTextExists('Arithmetic', 'page body contains "Arithmetic"');
-	test.assertTextExists('José', 'page body contains "José"');
-	test.assertTextExists('Çetin', 'page body contains "Çetin"');
+ 	test.assertTextExists('No items found', 'page no items found');
     }).run(function() {
         test.done();
     });
