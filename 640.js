d96d3bcbf525df37c4469c3a37b83ea2565bab23
Michael Cummings <michaelc@gwu.edu>
2016-02-04 18:34:53 -0500

    updated

--- 640.js
+++ 640.js
@@ -1,12 +1,11 @@
 // m27_001 #640
 //
-casper.test.begin('m27_001 #640 hathi trust online', 3, function suite(test) { 
+casper.test.begin('m27_001 #640 hathi trust online', 2, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/7269114';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
-	test.assertTextExists('Hathi Trust' , 'found Hathi Trust holding');
-	test.assertTextExists('000124204' , 'match Hathi record number');
+	test.assertTextExists('HathiTrust' , 'found Hathi Trust holding from wrlc');
 	test.assertTextExists('Treasure island' , 'matched title');
     }).run(function() {
         test.done();
