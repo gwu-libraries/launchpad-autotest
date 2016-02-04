b4e5555eb3cd17fe7b3694ebbecad131ee07ecfb
Michael Cummings <michaelc@gwu.edu>
2016-02-04 15:55:14 -0500

    update use case,bib not in WRLC

--- 435.js
+++ 435.js
@@ -1,14 +1,13 @@
 // m16_001 #435
 // previous test for issn value was invalid. 
+// finds record not in voyager
 //
-casper.test.begin('m16_001 #435 WRLC shared ebook', 3, function suite(test) { 
+casper.test.begin('m16_001 #435 ', 1, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/isbn/9781452220987';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
-	test.assertTextExists('Home visiting' , 'page Home visiting... found by ISBN query');
-	test.assertTextExists('Details' , 'Verified Details label');
-	test.assertTextExists('Electronic books' , 'Verified content of Genre');
+	test.assertTextExists('Home Visiting' , 'page Home Visiting... found by ISBN query');
     }).run(function() {
         test.done();
     });
