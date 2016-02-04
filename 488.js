f74dbd34671cc2341c91a9e98a5b9a419dd68fe4
Michael Cummings <michaelc@gwu.edu>
2016-02-04 18:33:08 -0500

    revised

--- 488.js
+++ 488.js
@@ -1,13 +1,13 @@
 // m16_003 #488, 577
 // 
 //
-casper.test.begin('m16_003 #488, 577 verify Internet Archive link', 2, function suite(test) { 
+casper.test.begin('m16_003 #488, 577 verify Internet Archive link omitted', 2, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/4403350';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
 	test.assertTextExists('The golden bough' , 'page title golden bough... found');
-	test.assertTextExists('Internet Archive' , 'Verified internet archive displayed');
+	test.assertTextDoesntExist('Internet Archive' , 'Verified internet archive displayed');
     }).run(function() {
         test.done();
     });
