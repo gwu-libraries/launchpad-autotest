0e1bbaae9c10d2dc958fe6d7e2b85f2051356d63
Michael Cummings <michaelc@gwu.edu>
2016-02-04 15:51:03 -0500

    update use case, bib deleted from WRLC

--- 400b.js
+++ 400b.js
@@ -1,13 +1,11 @@
 // m15_001 #400b 
 //
-casper.test.begin('m15_001 #400b Page other author and series title links', 3, function suite(test) { 
+casper.test.begin('m15_001 #400b Page other author and series title links', 1, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/9292523';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
-	test.assertTextExists('Model and data engineering' , 'page title Model and data... found');
-	test.assertSelectorHasText('a', 'Bellatreche, Ladjel, 1968-');
-	test.assertSelectorHasText('a', 'Lecture notes in computer science');
+	test.assertTextExists('No items found' , 'page no items found');
     }).run(function() {
         test.done();
     });
