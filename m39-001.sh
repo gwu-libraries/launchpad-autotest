032f241613b1e7def9f5865d49308299daf0cc26
Michael Cummings <cummings@gwdev-stgmike.wrlc.org>
2016-04-19 11:49:25 -0400

    add comments

--- m39-001.sh
+++ m39-001.sh
@@ -1,9 +1,11 @@
 # m39-001.sh
 # 
-# NOT tested 
+# NOT tested in casper yet 
 # 1120 sorted, de-duplicated subject terms
 # 1123 author date is part of the oclc identities link
 # 1124 google analytics is on the oclc identities link
+# 1129 remove debug statement
+# 1133 fix global variable name HttpError
 # 1141 himmelfarb links to their borrowing services page is ignored.
 #
 echo 'Starting m39-001 tests'
