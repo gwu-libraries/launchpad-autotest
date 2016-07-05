200115b959d14becb38f1f28cef196830c5067e3
Michael Cummings <cummings@gwdev-stgmike.wrlc.org>
2016-07-05 17:09:21 -0400

    remove test for deleted bib

--- 479.js
+++ 479.js
@@ -1,7 +1,7 @@
 // m15_003 #479, 482, 491
 // 
-//
-casper.test.begin('m15_003 #479, 482, 491', 5, function suite(test) { 
+// UPDATE July 2016, udc no longer holds. removed one test.
+casper.test.begin('m15_003 #479, 482, 491', 4, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/7869426'
 var ENTITY=SERVER+USECASE;
@@ -9,7 +9,6 @@ var ENTITY=SERVER+USECASE;
 	test.assertTextExists('American Journal of Food' , 'page title American Journal... found');
 	test.assertTextExists('George Washington' , 'Verified GW holding');
 	test.assertTextExists('Georgetown' , 'Verified GT holding');
-	test.assertTextExists('UDC' , 'Verified DC holding');
 	test.assertTextExists('Full ' , 'Verified Full text online ');
     }).run(function() {
         test.done();
