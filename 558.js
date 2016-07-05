0aefe3b8ed026ae27f627a8eb767579d0fc7b4b2
Michael Cummings <michaelc@gwu.edu>
2016-07-05 17:16:42 -0400

    revised - bib has been deleted

--- 558.js
+++ 558.js
@@ -1,12 +1,12 @@
 // m16_003 #558, 568
-// 
+// THIS USECASE NO LONGER IS IN THE VOYAGER DATABASE
 // the fact that a page is returned is sufficient to validate this test
 casper.test.begin('m16_003 #558, 568 diacritics in other author ', 1, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/item/13319347';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
-	test.assertTextExists('Cadernos' , 'page title Caderno do Programa... found');
+	test.assertTextExists('No items found' , 'no items. found');
     }).run(function() {
         test.done();
     });
