28eeacd14a1b9916d37f4e0465e2cf69b99000ae
cummingsm <cummings@template.wrlc.org>
2017-01-03 16:23:30 -0500

    update test

--- 1108.js
+++ 1108.js
@@ -1,12 +1,12 @@
 // m36-001 #1108 user more Worldcat description
-casper.test.begin('m36-001 #1108 user more Worldcat description', 3, function suite(test) { 
+casper.test.begin('m36-001 #1108 use more Google Books description', 3, function suite(test) { 
 casper.echo("Test environment: "+SERVER);
 var USECASE='/oclc/34473395';
 var ENTITY=SERVER+USECASE;
 	casper.start(ENTITY, function() {
 	test.assertTextExists('Brain' , 'matched title');
-	test.assertTextExists('from the OCLC WorldCat catalog' , 'matched string OCLC WorldCat catalog');
-	test.assertTextExists('Includes index', 'matched review text');
+	test.assertTextExists('from Google Books', 'matched string Google Books');
+	test.assertTextExists('Sandeman', 'matched name');
     }).run(function() {
         test.done();
     });
